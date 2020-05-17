5430ca4a27d854c604f5e771b0026717f914846d
r07631006 <r07631006@ntu.edu.tw>
2020-05-17 23:18:01 +0800

    self config

--- usage.py
+++ usage.py
@@ -1,5 +1,21 @@
 '''
 
+1. 修改 mmdetection/mmdet/core/evaluation 裡面 class_names.py 的 voc_classes
+
+2. 修改 mmdetection/mmdet/datasets/voc.py 裡的類別   [如果只有一類面要加逗號]) 
+
+3. 修改 config.py
+	
+	3.1 num_classes 要改
+ 
+	3.2  config.py  裡 dataset_type, dataroot, ann_file, img_prefix
+
+	lr set 4 GPU * 2 img/gpu = 8 img in mb --> lr = 0.01
+	       16    * 4         = 64              lr = 0.08
+	       1     * 2         = 2               lr = 0.0025
+
+
+
 python3 tools/train.py ${CONFIG_FILE} --work_dir ${YOUR_WORK_DIR} [optional arguments]
 
 # single-gpu testing
