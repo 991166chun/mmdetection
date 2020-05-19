b566386f9ce2cb1ce6f8274178fa75b6552b1419
r07631006 <r07631006@ntu.edu.tw>
2020-05-19 23:25:39 +0800

    chasnge 0519

--- configs/xm/faster_rcnn_r50_fpn.py
+++ configs/xm/faster_rcnn_r50_fpn.py
@@ -1,3 +1,9 @@
+_base_ = [
+     './my_voc.py',
+     './schedule_1x.py',
+    '../_base_/default_runtime.py'
+]
+
 model = dict(
     type='FasterRCNN',
     pretrained='torchvision://resnet50',
@@ -30,20 +36,21 @@ model = dict(
             target_stds=[1.0, 1.0, 1.0, 1.0]),
         loss_cls=dict(
             type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
-        loss_bbox=dict(type='L1Loss', loss_weight=1.0)),
+        loss_bbox=dict(type='SmoothL1Loss', beta=1.0,
+                               loss_weight=1.0)),
     roi_head=dict(
         type='StandardRoIHead',
         bbox_roi_extractor=dict(
             type='SingleRoIExtractor',
-            roi_layer=dict(type='RoIAlign', out_size=7, sample_num=0),
+            roi_layer=dict(type='RoIAlign', out_size=14, sample_num=0),
             out_channels=256,
             featmap_strides=[4, 8, 16, 32]),
         bbox_head=dict(
             type='Shared2FCBBoxHead',
             in_channels=256,
             fc_out_channels=1024,
-            roi_feat_size=7,
-            num_classes=80,
+            roi_feat_size=14,
+            num_classes=14,
             bbox_coder=dict(
                 type='DeltaXYWHBBoxCoder',
                 target_means=[0., 0., 0., 0.],
@@ -51,7 +58,8 @@ model = dict(
             reg_class_agnostic=False,
             loss_cls=dict(
                 type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
-            loss_bbox=dict(type='L1Loss', loss_weight=1.0))))
+            loss_bbox=dict(type='SmoothL1Loss', beta=1.0,
+                               loss_weight=1.0))))
 # model training and testing settings
 train_cfg = dict(
     rpn=dict(
---
+++ train.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=1 python3 -u tools/train.py configs/xm/faster_rcnn_r50_fpn.py
